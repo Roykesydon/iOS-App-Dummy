4fbc6adb49d8a3afc14d80bb211731c57b4fd533
roykesydone <roy23381547@gmail.com>
2022-09-25 00:18:42 +0800

    Initial Commit

---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by roykesydone on 2022/9/25.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
